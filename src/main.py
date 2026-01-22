34357d28c13d5535f5396ed34c015c9a04f5f06b
gsaujla <g.s.aujla@outlook.com>
2026-01-22 20:55:05 +0000

    file restructure

--- src/main.py
+++ src/main.py
@@ -1,50 +1,13 @@
-"""Main file to query polymarket."""
+"""Python Module to execute main block of code."""
 
-# import webbrowser
-# # webbrowser.open('https://polymarket.com/finance')
-
-import re
-import sys
-
-import bs4
-import requests  # type: ignore[import-untyped]
-
-GAMMA_API_URL = "https://gamma-api.polymarket.com/events"
-POLYMARKET_URL = "https://polymarket.com"
+from polymarket_scraper import get_price_value, query_web_page
 
 
-print("Searching...")
-headers = {"User-Agent": "Mozilla/5.0"}
-res = requests.get(POLYMARKET_URL + "/" + " ".join(sys.argv[1:]), headers=headers)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, "html.parser")
-web_page_data = soup.select("p")
+def main() -> None:
+    """Run main block of code."""
+    web_page_data = query_web_page()
+    get_price_value(web_page_data)
 
-matches = set(
-    [p for p in web_page_data if re.search(r"finish", p.get_text(), re.IGNORECASE)]
-)
 
-first_match = list(matches)[0]
-for p in matches:
-    parent_link = p.parent.parent.parent.parent
-    if parent_link and parent_link.name == "a":
-        parent_link = parent_link.get("href")
-        slug = parent_link.split("/")[-1]
-    else:
-        print("cant find link")
-
-    api_res = requests.get(f"{GAMMA_API_URL}?slug={slug}", headers=headers)
-
-    data = api_res.json()
-
-    if data:
-        event = data[0]
-        title = event.get("title")
-        print(f"Market: {title}")
-
-        for market in event.get("markets", []):
-            outcome_label = market.get("groupItemTitle", "Price")
-            raw_price = market.get("outcomePrices", ["0"])[0]
-            # Convert to dollar format
-            price = raw_price
-            print(f"  -> {outcome_label}")
+if __name__ == "__main__":
+    main()
